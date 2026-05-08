20e9f7f237d9a1203be812a23a7bacc06553016b
sherozxon-coder <sherozeshmamatov697@gmail.com>
2026-05-08 10:52:52 +0500

    form malumotlari olindi

--- js/index.js
+++ js/index.js
@@ -1,12 +1,36 @@
+const formEvent = document.querySelector('.form-event');
 const btnIncome = document.getElementById('btn-income');
 const btnExpense = document.getElementById('btn-expense');
+const inputMaosh = document.querySelector('.Input-maosh');
+const inputNUM = document.querySelector('.Input-NUM');
+
+let selectedType = "Daromad"; // default
 
 btnIncome.addEventListener('click', function () {
+    selectedType = "Daromad";
     btnIncome.className = 'type-btn active-income';
     btnExpense.className = 'type-btn';
 });
 
 btnExpense.addEventListener('click', function () {
+    selectedType = "Xarajat";
     btnExpense.className = 'type-btn active-expense';
     btnIncome.className = 'type-btn';
+});
+
+formEvent.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const select = document.getElementById("kategoriya");
+
+    const OBJ = {
+        type: selectedType,
+        kategory: select.value,
+        maosh: inputMaosh.value,
+        num: inputNUM.value,
+    };
+
+    formEvent.reset()
+
+    console.log(OBJ);
 });
